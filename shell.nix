f1684478108ee8f428c782ebc1131e2efad1f79b
jerbaroo <jerbaroo.work@pm.me>
2025-06-06 11:04:17 +0100

    test cache

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
 let
   pkgs = import ./packages.nix;
   project = import ./default.nix;
-in project.shellFor {
+in
+project.shellFor {
   exactDeps = true;
-  buildInputs =  [ pkgs.ghcid pkgs.postgresql ];
+  buildInputs = [
+    pkgs.ghcid
+    # pkgs.libpq
+    # pkgs.haskellPackages.libpq
+    # pkgs.haskellPackages.postgresql-libpq
+    pkgs.postgresql
+    pkgs.stack
+  ];
   tools = {
     cabal = "latest";
     haskell-language-server = "latest";

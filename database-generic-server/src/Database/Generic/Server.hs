70f15a4461e78a7ae82edb67328956e2c19d7b91
jerbaroo <jerbaroo.work@pm.me>
2025-06-08 20:37:51 +0100

    rm stack

--- database-generic-server/src/Database/Generic/Server.hs
+++ database-generic-server/src/Database/Generic/Server.hs
@@ -1,3 +1,7 @@
 module Database.Generic.Server where
 
+-- TODO server API to separate package
+import Servant.API
+
 x = 4
+
--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 let
   pkgs = import ./packages.nix;
-in pkgs.haskell-nix.project {
+in pkgs.haskell-nix.cabalProject {
+  compiler-nix-name = "ghc98";
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "database-generic";
     src = ./.;

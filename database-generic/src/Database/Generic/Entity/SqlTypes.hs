ae212206c70a496a80ce2b2a95bc06602397d775
jerbaroo <jerbaroo.work@pm.me>
2025-06-10 22:08:02 +0100

    cleaning up sql types

--- database-generic/src/Database/Generic/Entity/SqlTypes.hs
+++ database-generic/src/Database/Generic/Entity/SqlTypes.hs
@@ -1,4 +1,3 @@
--- TODO review
 module Database.Generic.Entity.SqlTypes where
 
 import Data.Aeson qualified as Aeson
@@ -7,61 +6,57 @@ import Database.Generic.Prelude
 import Database.HDBC qualified as H
 import Prelude (read)
 
--- | Names of types in SQL.
-data SqlType
-  = SqlBigInt
-  | SqlVarChar
+data DbType
+  = DbInt64
+  | DbString
   deriving (Eq, Generic, Show)
 
-instance Aeson.FromJSON SqlType
-instance Aeson.ToJSON   SqlType
+instance Aeson.FromJSON DbType
+instance Aeson.ToJSON   DbType
 
--- | Types that have a corresponding SQL type.
-class HasSqlType a where
-  sqlType :: SqlType
+class HasDbType a where
+  dbType :: DbType
 
--- TODO: should be done on per-db basis.
--- TODO: docs
-instance HasSqlType Int64 where
-  sqlType = SqlBigInt
+instance HasDbType Int64 where
+  dbType = DbInt64
 
-instance HasSqlType String where
-  sqlType = SqlVarChar
+instance HasDbType String where
+  dbType = DbString
 
-data SqlValue
-  = SqlByteString !SqlBS
-  | SqlInt64      !Int64
-  | SqlInteger    !Integer
-  | SqlString     !String
+data DbValue
+  -- = SqlByteString !SqlBS
+  = DbVInt64  !Int64
+  | DbVString !String
   deriving (Eq, Generic, Show)
 
-instance Aeson.FromJSON SqlValue
-instance Aeson.ToJSON   SqlValue
+instance Aeson.FromJSON DbValue
+instance Aeson.ToJSON   DbValue
 
-instance From H.SqlValue SqlValue where
-  from (H.SqlByteString b) = from b
-  from (H.SqlInt64 i) = from i
-  from (H.SqlInteger i) = from i
-  from (H.SqlString s) = from s
-  from x = error $ "SqlTypes.hs unmatched pattern on " <> show x
+-- -- TODO client shouldn't be HDBC-specific
+-- instance From H.SqlValue DbValue where
+--   from (H.SqlByteString b) = from b
+--   from (H.SqlInt64 i) = from i
+--   from (H.SqlInteger i) = from i
+--   from (H.SqlString s) = from s
+--   from x = error $ "SqlTypes.hs unmatched pattern on " <> show x
 
-instance From ByteString SqlValue where
-  from = SqlByteString . SqlBS
+-- instance From ByteString SqlValue where
+--   from = SqlByteString . SqlBS
 
-instance From Int64 SqlValue where
-  from = SqlInt64
+-- instance From Int64 SqlValue where
+--   from = SqlInt64
 
-instance From Integer SqlValue where
-  from = SqlInteger
+-- instance From Integer SqlValue where
+--   from = SqlInteger
 
-instance From String SqlValue where
-  from = SqlString
+-- instance From String SqlValue where
+--   from = SqlString
 
-newtype SqlBS = SqlBS ByteString deriving (Eq, Show)
+-- newtype SqlBS = SqlBS ByteString deriving (Eq, Show)
 
--- TODO: look at this. convert into SqlString?
-instance Aeson.FromJSON SqlBS where
-  parseJSON = fmap (SqlBS . read) <$> Aeson.parseJSON
+-- -- TODO: look at this. convert into SqlString?
+-- instance Aeson.FromJSON SqlBS where
+--   parseJSON = fmap (SqlBS . read) <$> Aeson.parseJSON
 
-instance Aeson.ToJSON SqlBS where
-  toJSON = Aeson.toJSON . show
+-- instance Aeson.ToJSON SqlBS where
+--   toJSON = Aeson.toJSON . show
